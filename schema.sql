55c8a993080e6913d80c5140e523abcb7b8c1207
MarianBezhenar <bezhenarmarian@gmail.com>
2026-04-08 17:04:53 +0200

    remove: assegnazioni

--- schema.sql
+++ schema.sql
@@ -12,20 +12,3 @@ CREATE TABLE prodotti (
     tipo VARCHAR(200) NOT NULL,
     data_produzione DATE NOT NULL
 );
-
--- ASSEGNAZIONI
-CREATE TABLE assegnazioni (
-    nickname VARCHAR(200) NOT NULL,
-    id_prodotto VARCHAR(9) NOT NULL,
-    PRIMARY KEY (nickname, id_prodotto),
-    CONSTRAINT fk_prodotto
-        FOREIGN KEY (id_prodotto)
-        REFERENCES prodotti(id_prodotto)
-        ON DELETE CASCADE
-        ON UPDATE CASCADE,
-    CONSTRAINT fk_user
-        FOREIGN KEY (nickname)
-        REFERENCES users(nickname)
-        ON DELETE CASCADE
-        ON UPDATE CASCADE
-);
